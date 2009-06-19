9096643cae12063037d40334ee656cfcf83b7192
Thomas Huber <thomas3577@users.noreply.github.com>
2009-06-19 15:13:15 +0000

    Delete unused Files

--- sources/System/Library/Users/Group.cs
+++ sources/System/Library/Users/Group.cs
@@ -1,31 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+//Sharpcms.net is licensed under the open source license GPL - GNU General Public License.
+
 using System.Xml;
 using InventIt.SiteSystem.Library;
 
 namespace InventIt.SiteSystem.Data.Users
 {
-
     public class Group : DataElement
     {
-        public string Name
+        public Group(XmlNode node)
+            : base(node)
         {
-            get
-            {
-                return CommonXml.GetAttributeValue(Node,"name");
-            }
-            set 
-            {
-                CommonXml.SetAttributeValue(Node, "name",Common.CleanToSafeString(value));
-            }
         }
 
-        public Group(XmlNode node)
-			: base(node)
+        public string Name
         {
-		}
-        
-
+            get { return CommonXml.GetAttributeValue(Node, "name"); }
+            set { CommonXml.SetAttributeValue(Node, "name", Common.CleanToSafeString(value)); }
+        }
     }
 }
--- sources/System/Library/Users/GroupList.cs
+++ sources/System/Library/Users/GroupList.cs
@@ -1,70 +1,73 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-using InventIt.SiteSystem.Library;
+//Sharpcms.net is licensed under the open source license GPL - GNU General Public License.
+
 using System.Xml;
+using InventIt.SiteSystem.Library;
 
 namespace InventIt.SiteSystem.Data.Users
 {
     public class GroupList : DataElementList
     {
         public GroupList(XmlNode parentNode)
-			: base(parentNode)
-		{
-		}
+            : base(parentNode)
+        {
+        }
 
-		public Group Create(string name)
-		{
-			XmlNode node = Document.CreateElement("group");
-			ParentNode.AppendChild(node);
-			Group group = new Group(node);
-            group.Name = name;
+        public Group this[int index]
+        {
+            get
+            {
+                string xPath = string.Format("group[{0}]", index + 1);
+                XmlNode node = ParentNode.SelectSingleNode(xPath);
+
+                if (node != null)
+                {
+                    var group = new Group(node);
+                    return group;
+                }
+                return null;
+            }
+        }
+
+        public Group this[string name]
+        {
+            // ToDo: group is not consistent yet (old)
+            get
+            {
+                string xPath = string.Format("group[@name='{0}']", Common.CleanToSafeString(name));
+                XmlNode node = ParentNode.SelectSingleNode(xPath);
+
+                if (node != null)
+                {
+                    var group = new Group(node);
+                    return group;
+                }
+                return null;
+            }
+        }
+
+        public Group Create(string name)
+        {
+            XmlNode node = Document.CreateElement("group");
+            ParentNode.AppendChild(node);
+            var group = new Group(node) {Name = name};
             return group;
-		}
-        
-        public void Remove(int index)
+        }
+
+        public void Remove(int index) //ToDo: Is a unused Method (T.Huber 18.06.2009)
         {
             Group group = this[index];
             ParentNode.RemoveChild(group.Node);
         }
+
         public void Remove(string name)
         {
             Group group = this[name];
             ParentNode.RemoveChild(group.Node);
         }
+
         public void Clear()
         {
             ParentNode.RemoveAll();
-        }
-		public Group this[int index]
-		{
-			get
-			{
-				string xPath = string.Format("group[{0}]", index + 1);
-				XmlNode node = ParentNode.SelectSingleNode(xPath);
-
-				if (node != null)
-				{
-                    Group group = new Group(node);
-                    return group;
-				}
-				return null;
-			}
-		}
-        public Group this[string name]
-        {// TODO: group is not consistent yet
-            get
-            {
-                string xPath = string.Format("group[@name='{0}']",Common.CleanToSafeString(name) );
-                XmlNode node = ParentNode.SelectSingleNode(xPath);
-
-                if (node != null)
-                {
-                    Group group = new Group(node);
-                    return group;
-                }
-                return null;
-            }
         }
     }
 }
